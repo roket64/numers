5ceb28b64cec21785285d973da638eb7e9ef315d
roket64 <1p41i4y1@gmail.com>
2023-11-12 01:33:44 +0900

    congr module added

---
+++ src/congr/mod.rs
@@ -0,0 +1,16 @@
+/// Struct for represent the solution of linear congruence ax = k (mod modulo)
+pub struct CongrSolution {}
+
+/// Struct that represents linear congruence `ax = n (mod m)`, where `coefficient` represents `a`, \
+/// `modulo` represents `m`.
+pub struct Congruence {
+    coefficient: u32,
+    n: u32,
+    modulo: u32,
+}
+
+impl Congruence {
+    pub fn solve() {
+        todo!()
+    }
+}
